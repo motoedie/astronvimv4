6ea368b4c2e3a730547c512037772d48dbc7972e
Ladislav Stárek <ladislav.starek@MacBookPro-MP7J4D6PK7.local>
2025-01-03 05:10:52 +0100

    Fix neovim colors

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -35,6 +35,7 @@ return {
         wrap = false, -- sets vim.opt.wrap
         list = true,
         colorcolumn = "120",
+        termguicolors = true,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
